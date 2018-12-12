7353bb80cf39ed55a86dad9fce35a14e8f32ec35
Jordan Whitten <jordanwa1947@gmail.com>
2018-12-12 10:25:41 -0700

    conn method work in progress

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -4,12 +4,22 @@ class NrelService
     @zip = zip_code
   end
 
+  def stations
+    get_json
+  end
+
   private
 
+  def get_json
+    binding.pry
+  end
+
   def conn
-    Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json' do |faraday|
+    Faraday.new(url: 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json') do |faraday|
 
       faraday.headers['api_key'] = ENV['NREL_KEY']
-    )
+      faraday.headers['format'] = 'JSON'
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
